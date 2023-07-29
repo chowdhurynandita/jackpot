dd16088023ac6f43880fbf3ad5d7fd5a53702696
Nandita Chowdhury <140270489+chowdhurynandita@users.noreply.github.com>
2023-07-29 18:22:38 +0530

    Created MirrorText

---
+++ MirrorText/MirrorText.java
@@ -0,0 +1,33 @@
+// TODO: Write a brief program description
+import java.util.*;
+
+class MirrorText
+{
+    public static void main(String args[])
+    {
+        // Input text
+        String text = input();
+
+        // Reverse text
+        String mirrorText = reverse(text);
+
+        // Display mirrored text
+        display(mirrorText);
+    }
+
+    // TODO: create function to input text
+
+    // Reverse text
+    public static String reverse(String text)
+    {
+        String mirrorText = "";
+
+        // TODO: Find text length
+
+        // TODO: Loop through the text starting from the end and concatenate each character to mirrorText
+
+        // Return mirrorText
+    }
+
+    // TODO: Create function to display mirrored text
+}
